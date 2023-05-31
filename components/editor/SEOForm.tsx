a29c65e84b839e838a774a14bd8cb20d257b4a76
Tibor22 <tothtibitravel@gmail.com>
2023-05-31 13:27:05 +0100

    implemented SEO

---
+++ components/editor/SEOForm.tsx
@@ -0,0 +1,96 @@
+import { ChangeEventHandler, FC, useEffect, useState } from 'react';
+import slugify from 'slugify';
+import classnames from 'classnames';
+
+export interface SeoResult {
+	meta: string;
+	slug: string;
+	tags: string;
+}
+
+interface Props {
+	title?: string;
+	onChange(result: SeoResult): void;
+}
+
+const commonInput =
+	'w-full bg-transparent outline-none border-2 border-secondary-dark focus:border-primary-dark focus:dark:border-primary rounded transition text-primary-dark dark:text-primary p-2';
+
+const Input: FC<{
+	name?: string;
+	value?: string;
+	placeholder?: string;
+	onChange?: ChangeEventHandler<HTMLInputElement>;
+	label: string;
+}> = ({ name, value, placeholder, label, onChange }) => (
+	<label className='block relative'>
+		<span className='absolute top-1/2 -translate-y-1/2 text-sm font-semibold text-primary-dark dark:text-primary pl-2'>
+			{label}
+		</span>
+		<input
+			type='text'
+			name={name}
+			placeholder={placeholder}
+			className={classnames(commonInput, 'italic pl-11')}
+			onChange={onChange}
+			value={value}
+		/>
+	</label>
+);
+
+const SEOForm: FC<Props> = ({ title = '', onChange }): JSX.Element => {
+	const [values, setValues] = useState({ meta: '', slug: '', tags: '' });
+
+	const handleChange: ChangeEventHandler<
+		HTMLInputElement | HTMLTextAreaElement
+	> = (e) => {
+		let { name, value } = e.target;
+		if (name === 'meta') value = value.substring(0, 150);
+
+		setValues((prevValues) => ({ ...prevValues, [name]: value }));
+		onChange({ ...values, [name]: value });
+	};
+
+	useEffect(() => {
+		const slug = slugify(title.toLowerCase());
+		setValues((prevValues) => ({ ...prevValues, slug }));
+		onChange({ ...values, slug });
+	}, [title]);
+
+	const { meta, slug, tags } = values;
+	return (
+		<div className='space-y-4'>
+			<h1 className='text-primary-dark dark:text-primary text-xl font-semibold'>
+				SEO Section
+			</h1>
+			<Input
+				value={slug}
+				name='slug'
+				placeholder='slug-goes-here'
+				label='Slug:'
+				onChange={handleChange}
+			/>
+			<Input
+				value={tags}
+				name='tags'
+				placeholder='React, Next JS'
+				label='Tags:'
+				onChange={handleChange}
+			/>
+			<div className='relative'>
+				<textarea
+					onChange={handleChange}
+					value={meta}
+					name='meta'
+					className={classnames(commonInput, 'text-lg h-20 resize-none')}
+					placeholder='Meta description 150 characters will be fine'
+				></textarea>
+				<p className='absolute bottom-3 right-3 text-primary-dark dark:text-primary text-sm'>
+					{meta.length}/150
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default SEOForm;
--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -10,11 +10,30 @@ import TipTapImage from '@tiptap/extension-image';
 import EditLink from './Link/EditLink';
 import GalleryModal from './GalleryModal';
 import { ImageSelectionResult } from './GalleryModal';
+import axios from 'axios';
+import SEOForm from './SEOForm';
 interface Props {}
 
 const Editor: FC<Props> = (props): JSX.Element => {
 	const [selectionRange, setSelectionRange] = useState<Range>();
 	const [showGallery, setShowGallery] = useState(false);
+	const [images, setImages] = useState<{ src: string }[]>([]);
+	const [uploading, setUploading] = useState(false);
+
+	const fetchImages = async () => {
+		const { data } = await axios('/api/image');
+		setImages(data.images);
+	};
+
+	const handleImageUpload = async (image: File) => {
+		setUploading(true);
+		const formData = new FormData();
+		formData.append('image', image);
+		const { data } = await axios.post('/api/image', formData);
+		setImages((prevImages) => [...prevImages, data]);
+		setUploading(false);
+	};
+
 	const editor = useEditor({
 		extensions: [
 			StarterKit,
@@ -72,22 +91,41 @@ const Editor: FC<Props> = (props): JSX.Element => {
 			editor.commands.setTextSelection(selectionRange);
 		}
 	}, [editor, selectionRange]);
+	useEffect(() => {
+		fetchImages();
+	}, []);
+
 	return (
 		<>
 			<div className='p-3 dark:bg-primary-dark bg-primary transition'>
+				<input
+					type='text'
+					placeholder='Title'
+					className=' py-2 outline-none bg-transparent w-full border-0 border-b-[1px] border-secondary-dark dark:border-secondary-light text-3xl font-semibold italic text-primary-dark dark:text-primary mb-3'
+				/>
 				<ToolBar
 					editor={editor}
 					onOpenImageClick={() => setShowGallery(true)}
 				/>
 				<div className='h-[1px] w-full bg-secondary-dark dark:bg-secondary-light my-3'></div>
 				{editor ? <EditLink editor={editor} /> : null}
-				<EditorContent editor={editor} />
+				<EditorContent editor={editor} className='min-h-[300px]' />
+				<div className='h-[1px] w-full bg-secondary-dark dark:bg-secondary-light my-3'></div>
+				<SEOForm
+					onChange={(result) => {
+						console.log(result);
+					}}
+					title={'This is my title'}
+				/>
 			</div>
 			{showGallery && (
 				<GalleryModal
+					images={images}
 					visible={showGallery}
 					onClose={() => setShowGallery(false)}
 					onSelect={handleImageSelection}
+					onFileSelect={handleImageUpload}
+					uploading={uploading}
 				/>
 			)}
 		</>
